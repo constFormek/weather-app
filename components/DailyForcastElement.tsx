96b17e55755f7911121f69a4f04e5321fb7c21cd
plexi <plexify.code@gmail.com>
2025-11-03 00:17:10 +0100

    created daily-forecast-element component defined it's interface copied iconmap and icon imports, need to move it to other file later used it in layout.tsx

---
+++ components/DailyForcastElement.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+
+import sunIcon from "@/public/assets/images/icon-sunny.webp";
+import drizzleIcon from "@/public/assets/images/icon-drizzle.webp";
+import fogIcon from "@/public/assets/images/icon-fog.webp";
+import overcastIcon from "@/public/assets/images/icon-overcast.webp";
+import rainIcon from "@/public/assets/images/icon-rain.webp";
+import snowIcon from "@/public/assets/images/icon-snow.webp";
+import stormIcon from "@/public/assets/images/icon-storm.webp";
+import partlyCloudyIcon from "@/public/assets/images/icon-partly-cloudy.webp"
+
+const iconMap = {
+    sunny: sunIcon,
+    drizzle: drizzleIcon,
+    fog: fogIcon,
+    overcast: overcastIcon,
+    rain: rainIcon,
+    snow: snowIcon,
+    storm: stormIcon,
+    "partly-cloudy": partlyCloudyIcon,
+};
+
+type iconType = 
+| "sunny"
+| "drizzle"
+| "fog"
+| "overcast"
+| "rain"
+| "snow"
+| "storm"
+| "partly-cloudy";
+
+interface DailyForcastElementProps {
+  iconName: iconType,
+  day: "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday",
+  tempDay: number,
+  tempNight: number
+}
+
+const DailyForcastElement = ({ iconName, day, tempDay, tempNight}: DailyForcastElementProps) => {
+  const icon = iconMap[iconName];
+  const validatedDay = day.substring(0,3).charAt(0).toUpperCase() + day.slice(1, 3);
+  return (
+    <div className="flex flex-col gap-2 bg-neutral-800 border border-neutral-600 rounded-xl px-2 py-3 w-25 items-center">
+      <h3 className="text-center text-[18px]">{validatedDay}</h3>
+
+      <Image src={icon} alt="" width={50} height={50}/>
+
+      <div className="flex justify-between items-center w-full">
+        <p>{tempDay}°</p>
+
+        <p>{tempNight}°</p>
+      </div>
+    </div>
+  )
+}
+
+export default DailyForcastElement;
