8934144fd4be39a54f2367a6cf5862a26fca0088
plexi <plexify.code@gmail.com>
2025-11-04 18:54:22 +0100

    fully styled dropdown wrapper component

---
+++ components/DropdownWrapper.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+interface DropdownWrapperProps {
+    children: ReactNode
+}
+
+const DropdownWrapper = ({children}: DropdownWrapperProps) => {
+  return (
+    <div className="w-full bg-neutral-800 border border-neutral-700 flex flex-col gap-1 rounded-xl p-2">
+        {children}
+    </div>
+  )
+}
+
+export default DropdownWrapper
